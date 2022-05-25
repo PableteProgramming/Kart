67badfa55339155766c9fee6f3ce14f5f28b4fb2
PableteProgramming <70910514+PableteProgramming@users.noreply.github.com>
2022-05-25 16:23:20 +0200

    Moving capsule forwards and backwards

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private bool ctrl;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ctrl = false;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            ctrl = true;
+        }
+        else
+        {
+            ctrl = false;
+        }
+
+        if (Input.GetKey("space"))
+        {
+            if (ctrl)
+            {
+                transform.position += -transform.forward * speed * Time.deltaTime;
+            }
+            else
+            {
+                // move forwards
+                transform.position += transform.forward * speed * Time.deltaTime;
+            }
+        }
+    }
+}
