51827ea5bfffbcf5c84f0d23582f0f8498481da1
PableteProgramming <70910514+PableteProgramming@users.noreply.github.com>
2022-05-28 20:53:21 +0200

    Adding camera script that follows player

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject follow_obj;
+    private float init_x;
+    private float init_z;
+    private float init_y;
+
+    void Start()
+    {
+        init_x = transform.position.x;
+        init_z = transform.position.z;
+        init_y = transform.position.y;
+    }
+
+    void Update()
+    {
+        transform.position = new Vector3(follow_obj.transform.position.x + init_x, follow_obj.transform.position.y + init_y, follow_obj.transform.position.z + init_z);
+    }
+}
